263e29e3da84772ff72f9891aadd8c12a8e92a8f
ndaruvala <ndaruvala2346@gmail.com>
2023-06-05 15:56:36 -0400

    Add initial Spring Boot API

---
+++ src/test/java/com/ascend/ascend/AscendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascend.ascend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AscendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
