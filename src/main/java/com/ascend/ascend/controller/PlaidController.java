38c4e5a629683af56f3328c2654e15bf05b20548
ndaruvala <ndaruvala2346@gmail.com>
2023-09-16 14:26:19 -0400

    feat: Add plaid link token changes

--- src/main/java/com/ascend/ascend/controller/PlaidController.java
+++ src/main/java/com/ascend/ascend/controller/PlaidController.java
@@ -1,15 +1,16 @@
 package com.ascend.ascend.controller;
 
 import com.ascend.ascend.service.UserService;
-import com.plaid.client.model.CountryCode;
-import com.plaid.client.model.LinkTokenCreateRequest;
-import com.plaid.client.model.LinkTokenCreateRequestUser;
-import com.plaid.client.model.LinkTokenCreateResponse;
+import com.plaid.client.ApiClient;
+import com.plaid.client.model.*;
 import com.plaid.client.request.PlaidApi;
+import okhttp3.ResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.*;
+import retrofit2.Call;
 import retrofit2.Response;
 
 
@@ -19,17 +20,30 @@ import java.util.*;
 @RestController
 @RequestMapping("/plaid")
 public class PlaidController {
-   @Autowired
-   UserService userService;
+    UserService userService;
 
-   private PlaidApi plaidClient;
+    @Value("#{environment.PLAID_SECRET}")
+    private String plaidSecret;
 
-   @Value("#{environment.PLAID_SECRET}")
-   private String plaidSecret;
-   private PlaidController(UserService userService) {}
+    @Value("#{environment.PLAID_CLIENT_ID}")
+    private String plaidClientId;
 
-   @PostMapping("/{user_id}/get_link_token")
-   public String getLinkToken(@PathVariable String user_id) {
+    private PlaidApi plaidClient;
+
+    private PlaidController(UserService userService) {
+        this.userService = userService;
+
+        HashMap<String, String> apiKeys = new HashMap<String, String>();
+        apiKeys.put("clientId", plaidClientId);
+        apiKeys.put("secret", plaidSecret);
+
+        ApiClient apiClient = new ApiClient(apiKeys);
+        apiClient.setPlaidAdapter(ApiClient.Sandbox);
+        plaidClient = apiClient.createService(PlaidApi.class);
+    }
+
+    @PostMapping("/{user_id}/get_link_token")
+    public String getLinkToken(@PathVariable String user_id) {
        LinkTokenCreateRequestUser linkTokenCreateRequestUser = new LinkTokenCreateRequestUser()
                .clientUserId(user_id.toString());
 
@@ -37,8 +51,10 @@ public class PlaidController {
                .secret(plaidSecret)
                .clientName("Ascend")
                .language("en")
-               .clientId(user_id.toString())
-               .countryCodes(Arrays.asList(CountryCode.US));
+               .clientId(plaidClientId)
+               .countryCodes(Arrays.asList(CountryCode.US))
+               .user(linkTokenCreateRequestUser);
+
        Response<LinkTokenCreateResponse> linkTokenCreateResponse = null;
        try {
            linkTokenCreateResponse = plaidClient
@@ -54,5 +70,16 @@ public class PlaidController {
            System.out.println("Failed to create link token");
            return null;
        }
-   }
+    }
+
+    @PostMapping("/{user_id}/exchange_public_tokenreceive_public_token")
+    public String exchangePublicToken(@RequestBody String publicToken @PathVariable String user_id) {
+        ItemPublicTokenExchangeRequest itemPublicTokenExchangeRequest = new ItemPublicTokenExchangeRequest()
+            .publicToken(publicToken);
+        
+        ItemPublicTokenExchangeResponse itemPublicTokenExchangeResponse = plaidClient
+            .itemPublicTokenExchange(itemPublicTokenExchangeRequest)
+            .execute();
+    }
 }
+
